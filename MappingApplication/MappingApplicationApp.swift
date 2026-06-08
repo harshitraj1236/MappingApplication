7c741e35353fa702062c7fb4b34f35126f810d4d
Harshit Raj <harshitraj1236@gmail.com>
2026-06-08 23:53:48 +0530

    Initial Commit

---
+++ MappingApplication/MappingApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MappingApplicationApp.swift
+//  MappingApplication
+//
+//  Created by Harshit Raj on 08/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MappingApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
